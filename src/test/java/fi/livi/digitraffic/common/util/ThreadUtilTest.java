07f81f6663f2aa9de58542eb1d908776d6138ed3
jouniso <jouniso@users.noreply.github.com>
2023-11-01 13:47:45 +0100

    DPO-2091 Apien versiointi - käyttämättömän koodin poisto

--- src/test/java/fi/livi/digitraffic/common/util/ThreadUtilTest.java
+++ src/test/java/fi/livi/digitraffic/common/util/ThreadUtilTest.java
@@ -12,12 +12,10 @@ public class ThreadUtilTest {
 
     @Test
     public void delayMs() {
-        System.out.println(Thread.activeCount());
         IntStream.range(0,4).parallel().forEach(i -> {
             final StopWatch duration = StopWatch.createStarted();
             final int delayMs = TestUtil.getRandom(500, 1500);
             ThreadUtil.delayMs(delayMs);
-            System.out.println(Thread.activeCount());
             AssertUtil.assertGe(duration.getTime(), delayMs, 500);
         });
     }
