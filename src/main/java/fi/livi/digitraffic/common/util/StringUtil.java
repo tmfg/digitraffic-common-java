971290388d7760095e6f3a3a74ce35eb754cef06
jouniso <jouniso@users.noreply.github.com>
2024-08-16 13:12:35 +0200

    DPO-1599 Tie&Meri java-sovellusten riippuvuuksien päivitys - moved StringUtilTest to common form meri and road

--- src/main/java/fi/livi/digitraffic/common/util/StringUtil.java
+++ src/main/java/fi/livi/digitraffic/common/util/StringUtil.java
@@ -1,8 +1,23 @@
 package fi.livi.digitraffic.common.util;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.slf4j.helpers.MessageFormatter;
+import org.springframework.stereotype.Service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+
+@Service // Just to initialize ObjectWriter
 public class StringUtil {
+    private static final Logger log = LoggerFactory.getLogger(StringUtil.class);
+
+    private static ObjectWriter jsonObjectWriter;
+
+    public StringUtil(final ObjectMapper objectMapper) {
+        jsonObjectWriter = objectMapper.writerWithDefaultPrettyPrinter();
+    }
 
     /**
      * Performs string formation with log style messagePattern.
@@ -14,5 +29,34 @@ public class StringUtil {
      */
     public static String format(final String messagePattern, final Object... parameters) {
         return MessageFormatter.arrayFormat(messagePattern, parameters).getMessage();
+    }
+
+
+    public static String toJsonString(final Object o) {
+        try {
+            return jsonObjectWriter.writeValueAsString(o);
+        } catch (final JsonProcessingException e) {
+            log.error("Failed to convert object to JSON-string", e);
+        }
+        return o.toString();
+    }
+
+    public static String toJsonStringLogSafe(final Object o) {
+        if (o == null) {
+            return null;
+        }
+        try {
+            return padKeyValuePairsEqualitySignWithSpaces(jsonObjectWriter.writeValueAsString(o));
+        } catch (final JsonProcessingException e) {
+            log.error("Failed to convert object to JSON-string", e);
+            return padKeyValuePairsEqualitySignWithSpaces(o.toString());
+        }
+    }
+
+    public static String padKeyValuePairsEqualitySignWithSpaces(final String value) {
+        if (value != null) {
+            return value.replace("=", " = ");
+        }
+        return value;
     }
 }
--- src/test/java/fi/livi/digitraffic/common/util/StringUtilTest.java
+++ src/test/java/fi/livi/digitraffic/common/util/StringUtilTest.java
@@ -1,9 +1,20 @@
 package fi.livi.digitraffic.common.util;
 
+import java.util.UUID;
+
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 public class StringUtilTest {
+
+    @BeforeAll
+    static void beforeAll() {
+        new StringUtil(new ObjectMapper());
+    }
 
     @Test
     public void format() {
@@ -11,4 +22,49 @@ public class StringUtilTest {
         Assertions.assertEquals("foo bar", StringUtil.format("foo bar", "plaa"));
     }
 
+    @Test
+    public void toJsonStringAndToJsonStringLogSafe() {
+        final int number = (int) (Math.random() * 10);
+        final String text = UUID.randomUUID().toString();
+        final MyObject object = new MyObject(text, number);
+        final String expected =
+            "{\n" +
+            "  \"text\" : \"" + text + "\",\n" +
+            "  \"number\" : " + number + "\n" +
+            "}";
+        Assertions.assertEquals(expected, StringUtil.toJsonString(object));
+        Assertions.assertEquals(expected, StringUtil.toJsonStringLogSafe(object));
+    }
+
+    @Test
+    public void toJsonStringLogSafe() {
+        final int number = (int) (Math.random() * 10);
+        final String text = "value=" + UUID.randomUUID();
+        final MyObject object = new MyObject(text, number);
+        final String expected =
+            "{\n" +
+                "  \"text\" : \"" + text.replace("=", " = ") + "\",\n" +
+                "  \"number\" : " + number + "\n" +
+                "}";
+        Assertions.assertEquals(expected, StringUtil.toJsonStringLogSafe(object));
+    }
+
+    @Test
+    public void padKeyValuePairsEqualitySignWithSpaces() {
+        final String text = "a=b, c=d, e = f";
+        final String expected = "a = b, c = d, e  =  f";
+        Assertions.assertEquals(expected, StringUtil.padKeyValuePairsEqualitySignWithSpaces(text));
+    }
+
+    static class MyObject {
+        @JsonProperty
+        private final String text;
+        @JsonProperty
+        private final Integer number;
+
+        public MyObject(final String text, final Integer number) {
+            this.text = text;
+            this.number = number;
+        }
+    }
 }
