f35ca3e9c74087e91d1ae47c3dd7ab6a64b59af3
jouniso <jouniso@users.noreply.github.com>
2024-08-26 12:15:47 +0200

    DPO-2584 JMS-yhteyden korvaus Apache ActiveMQ Artemis JMS:llä - Move NotTransactionalServiceMethod annotation - Added ConditionalOnPropertyNotBlank annotation

---
+++ src/main/java/fi/livi/digitraffic/common/annotation/ConditionalOnPropertyNotBlank.java
@@ -0,0 +1,73 @@
+/*
+ * Copyright 2012-2024 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package fi.livi.digitraffic.common.annotation;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import org.springframework.context.annotation.Conditional;
+
+/**
+ * {@link Conditional @Conditional} that checks if the specified property have non blank value.
+ * Copied partly from {@link org.springframework.boot.autoconfigure.condition.ConditionalOnProperty}
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ ElementType.TYPE, ElementType.METHOD })
+@Documented
+@Conditional(OnPropertyNotBlankCondition.class)
+public @interface ConditionalOnPropertyNotBlank {
+
+	/**
+	 * Alias for {@link #name()}.
+	 * @return the names
+	 */
+	String[] value() default {};
+
+	/**
+	 * A prefix that should be applied to each property. The prefix automatically ends
+	 * with a dot if not specified. A valid prefix is defined by one or more words
+	 * separated with dots (e.g. {@code "foo.bar"}).
+	 * @return the prefix
+	 */
+	String prefix() default "";
+
+	/**
+	 * The name of the properties to test. If a prefix has been defined, it is applied to
+	 * compute the full key of each property. For instance if the prefix is
+	 * {@code app.config} and one value is {@code my-value}, the full key would be
+	 * {@code app.config.my-value}
+	 * <p>
+	 * Use the dashed notation to specify each property, that is all lower case with a "-"
+	 * to separate words (e.g. {@code my-long-property}).
+	 * <p>
+	 * If multiple names are specified, all of the properties have to pass the test for
+	 * the condition to match.
+	 * @return the names
+	 */
+	String[] name() default {};
+
+	/**
+	 * Specify if the condition should match if the property is not set. Defaults to
+	 * {@code false}.
+	 * @return if the condition should match if the property is missing
+	 */
+	boolean matchIfMissing() default false;
+
+}
