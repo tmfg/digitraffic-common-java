df2f40ee264e8e9593b574c4006f1bf1ce9990e5
Ilari Junnonen <ilari.junnonen@solita.fi>
2024-12-20 12:48:22 +0200

    DPO-2872 add resolver for filtering out null values when parameter is a list

---
+++ src/main/java/fi/livi/digitraffic/common/annotation/CustomRequestParam.java
@@ -0,0 +1,12 @@
+package fi.livi.digitraffic.common.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.PARAMETER)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface CustomRequestParam {
+    String defaultValue() default "";
+}
---
+++ src/main/java/fi/livi/digitraffic/common/config/resolvers/NullFilteringVarargsResolver.java
@@ -0,0 +1,46 @@
+package fi.livi.digitraffic.common.config.resolvers;
+
+import java.lang.reflect.Array;
+import java.util.stream.Stream;
+
+import org.springframework.core.MethodParameter;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+import fi.livi.digitraffic.common.annotation.CustomRequestParam;
+
+public class NullFilteringVarargsResolver implements HandlerMethodArgumentResolver {
+
+    // the resolver is run if parameter is annotated with @CustomRequest and is of type enum
+    @Override
+    public boolean supportsParameter(final MethodParameter parameter) {
+        return parameter.hasParameterAnnotation(CustomRequestParam.class) &&
+            parameter.getParameterType().getComponentType().isEnum();
+    }
+
+    @Override
+    public Object[] resolveArgument(final MethodParameter parameter, final ModelAndViewContainer mavContainer,
+                                    final NativeWebRequest webRequest, final WebDataBinderFactory binderFactory) {
+        final String[] parameterValues = webRequest.getParameterValues(parameter.getParameterName());
+        if (parameterValues == null) {
+            return null;
+        }
+        final Class<?> componentType = parameter.getParameterType().getComponentType();
+
+        // filter nulls and empty strings from array of parameter values
+        // before passing them on to controller method
+        return Stream.of(parameterValues)
+            .filter(value -> value != null && !value.isEmpty())
+            .map(value -> convertValue(value, componentType))
+            .toArray(size -> (Object[]) Array.newInstance(componentType, size));
+    }
+
+    private Object convertValue(final String value, final Class<?> targetType) {
+        if (Enum.class.isAssignableFrom(targetType)) {
+            return Enum.valueOf((Class<Enum>) targetType, value);
+        }
+        throw new IllegalArgumentException("Unsupported target type: " + targetType);
+    }
+}
